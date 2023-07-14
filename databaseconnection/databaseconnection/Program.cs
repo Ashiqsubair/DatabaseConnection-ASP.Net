da338e2bff07d13c43b4be90a17c0bfdd65a0af2
ASHIQ S <72742730+Ashiqsubair@users.noreply.github.com>
2023-07-14 10:35:48 +0530

    Add files via upload

---
+++ databaseconnection/databaseconnection/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using MySqlConnector;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connStr = "server=localhost;user=root;database=sys;port=3306;password=root";
+        string sql = "SELECT * FROM employee";
+        try
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            Console.WriteLine("Connecting to the database");
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetInt16(0)}, {reader.GetString(1)}");
+            }
+
+            reader.Close();
+            conn.Close();
+
+            Console.WriteLine("Connection closed");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
